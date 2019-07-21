3cc948965e97a69e8166500e2c87b8d9ce69b716
dalikim <dalikim@parti.xyz>
2019-07-22 05:20:52 +0900

    기술적인 수정 - 번들러 버전을 서버에 맞춥니다

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,2 +1,4 @@
 run "rm -rf #{config.release_path}/public/uploads"
 run "ln -nfs #{config.shared_path}/public/uploads #{config.release_path}/public/uploads"
+run "rm -rf #{config.release_path}/private"
+run "ln -nfs #{config.shared_path}/private #{config.release_path}/private"
