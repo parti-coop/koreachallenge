176c87ab7f1e0ee7d577f4a70d3e25a168347341
dalikim <dalikim@parti.xyz>
2019-07-30 13:22:50 +0900

    에디터의 이미지 용량은 10메가로 늘립니다

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,4 @@
 class Image < ApplicationRecord
   mount_uploader :file, DefaultImageUploader
-  validates :file, file_size: { less_than_or_equal_to: 1.megabytes }
+  validates :file, file_size: { less_than_or_equal_to: 10.megabytes }
 end
